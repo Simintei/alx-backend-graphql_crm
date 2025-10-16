cc7448b67098ffff1a1804ed7948adfe24eaa2dd
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-10-16 23:57:51 +0300

    Update settings.py

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #other apps
     'crm',
     'graphene_django',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
