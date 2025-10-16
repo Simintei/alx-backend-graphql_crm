c6bf02a928ceb9004265b2b6c5b2382940858e84
Mary Karanja <57101536+Simintei@users.noreply.github.com>
2025-10-16 23:00:55 +0300

    Add Mutation class to schema

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -3,5 +3,8 @@ import graphene
 class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
 
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
 schema = graphene.Schema(query=Query)
 
